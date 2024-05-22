cd98de3d7a2647c324fa5ec0e37814dd7e6da386
Abhishek Kumar <Abhishek Kumar@ABHISHEK-KUMAR->
2024-05-22 11:22:43 +0530

    Add project files.

---
+++ ModelInAspCore/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelInAspCore.Models;
+using ModelsInAspCore.Models;
+using System.Diagnostics;
+
+namespace ModelInAspCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var students = new List<StudentModel>
+            {
+                new StudentModel {rollNo = 1, Name = ""}
+            };
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ModelInAspCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ModelInAspCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
